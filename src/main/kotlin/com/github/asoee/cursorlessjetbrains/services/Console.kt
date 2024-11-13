b786224964a92d11c3d2bf55a2d34b4ea0ac7c1e
Anders Søe <anders@soee.org>
2024-11-13 07:29:02 +0100

    first working calls to cursorless

---
+++ src/main/kotlin/com/github/asoee/cursorlessjetbrains/services/Console.kt
@@ -0,0 +1,14 @@
+package com.github.asoee.cursorlessjetbrains.services
+
+import com.intellij.openapi.diagnostic.thisLogger
+
+class Console {
+
+    fun log(args: String) {
+//        var logline = ""
+//        for (s in args) {
+//            logline += s
+//        }
+      thisLogger().warn("" + args)
+    }
+}
--- src/main/kotlin/com/github/asoee/cursorlessjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/asoee/cursorlessjetbrains/services/MyProjectService.kt
@@ -1,6 +1,7 @@
 package com.github.asoee.cursorlessjetbrains.services
 
 import com.dokar.quickjs.QuickJs
+import com.dokar.quickjs.binding.define
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -18,14 +19,41 @@ class MyProjectService(project: Project) {
         thisLogger().info(MyBundle.message("projectService", project.name))
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
 
-        val talonJs = javaClass.getResource("/cursorless/index.cjs").readText()
+        val talonJs = javaClass.getResource("/cursorless/cursorless.js").readText()
         thisLogger().warn("talon js script size: " + talonJs.length)
         quickJs =  QuickJs.create(Dispatchers.Default)
         runBlocking {
-            quickJs.evaluate<Void>(code = talonJs, asModule = false)
-            quickJs.evaluate<Void>(code = "function calc(a,b) {return a*b;}")
+            initCursorless(talonJs)
         }
     }
 
+    private suspend fun initCursorless(talonJs: String) {
+        thisLogger().warn("compile...")
+        val compiled = quickJs.compile(talonJs, "cursorless.js", true)
+        thisLogger().warn("addmodule...")
+        quickJs.addModule(bytecode = compiled)
+
+        quickJs.define<Console>("console", Console())
+
+        thisLogger().warn("eval 1...")
+        //            quickJs.evaluate<Void>(code = talonJs, asModule = true, )
+        thisLogger().warn("eval 2...")
+        //            quickJs.evaluate<Void>(code = "function calc(a,b) {return a*b;}")
+        thisLogger().warn("process")
+        //            quickJs.evaluate<Void>(code = "var process = { env: { NODE_ENV:  \"dev\" } };", asModule = true)
+        thisLogger().warn("log")
+        //            quickJs.evaluate<Void>(code = "console.log('fooo');")
+        //            thisLogger().warn("log process")
+        //            quickJs.evaluate<Void>(code = "console.log(process);")
+        thisLogger().warn("import entry")
+        quickJs.evaluate<Void>(code = "const { entry } = await import('./cursorless.js');")
+        thisLogger().warn("exec entry")
+        quickJs.evaluate<Any>(code = "entry();")
+        thisLogger().warn("done")
+    }
+
     fun getRandomNumber() = (1..100).random()
+
+
 }
+
