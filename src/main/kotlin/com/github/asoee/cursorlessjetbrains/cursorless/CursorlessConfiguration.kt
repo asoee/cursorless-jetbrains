b8c9dc7aca6450994885d32b76c2fc52e7545119
Anders Søe <anders@soee.org>
2025-08-03 14:42:41 +0200

    add missing hashcode and equals

--- src/main/kotlin/com/github/asoee/cursorlessjetbrains/cursorless/CursorlessConfiguration.kt
+++ src/main/kotlin/com/github/asoee/cursorlessjetbrains/cursorless/CursorlessConfiguration.kt
@@ -16,7 +16,33 @@ data class CursorlessConfiguration(
     val decorationDebounceDelayMs: Int,
     val commandHistory: Boolean,
     val debug: Boolean,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CursorlessConfiguration
+
+        if (decorationDebounceDelayMs != other.decorationDebounceDelayMs) return false
+        if (commandHistory != other.commandHistory) return false
+        if (debug != other.debug) return false
+        if (tokenHatSplittingMode != other.tokenHatSplittingMode) return false
+        if (!wordSeparators.contentEquals(other.wordSeparators)) return false
+        if (experimental != other.experimental) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = decorationDebounceDelayMs
+        result = 31 * result + commandHistory.hashCode()
+        result = 31 * result + debug.hashCode()
+        result = 31 * result + tokenHatSplittingMode.hashCode()
+        result = 31 * result + wordSeparators.contentHashCode()
+        result = 31 * result + experimental.hashCode()
+        return result
+    }
+}
 
 @Serializable
 data class ExperimentalConfiguration(
@@ -30,7 +56,27 @@ data class TokenHatSplittingMode(
     val preserveCase: Boolean,
     val lettersToPreserve: Array<String>,
     val symbolsToPreserve: Array<String>,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TokenHatSplittingMode
+
+        if (preserveCase != other.preserveCase) return false
+        if (!lettersToPreserve.contentEquals(other.lettersToPreserve)) return false
+        if (!symbolsToPreserve.contentEquals(other.symbolsToPreserve)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = preserveCase.hashCode()
+        result = 31 * result + lettersToPreserve.contentHashCode()
+        result = 31 * result + symbolsToPreserve.contentHashCode()
+        return result
+    }
+}
 
 val DEFAULT_CONFIGURATION = CursorlessConfiguration(
     tokenHatSplittingMode = TokenHatSplittingMode(
